cf1de0eb2f12d3b583f20f094589e1708df930bf
Corentin Grall <corentin.grall@gmail.com>
2020-02-08 18:25:39 +0100

    finished lol

--- server.js
+++ server.js
@@ -10,3 +10,15 @@ const token = process.env.TELEGRAM_TOKEN
 if (!token) {
   throw new Error('Missing Telegram Token. Please provid a valid in .env file.')
 }
+
+const bot = new TelegramBot(token, { polling: true })
+
+bot.onText(/\/ac/, async (msg, match) => {
+  const chatId = msg.chat.id
+  const arguments = match.input.split(' ')
+  arguments.shift()
+  const item = Math.floor(Math.random() * 10)
+  let url = `https://minecraftskinstealer.com/achievement/${item}/Achievement+Get/`
+  arguments.forEach(argument => (url = `${url}${argument}+`))
+  bot.sendPhoto(chatId, url)
+})
